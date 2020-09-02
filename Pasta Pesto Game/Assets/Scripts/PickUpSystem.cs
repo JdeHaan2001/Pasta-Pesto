0db0ce1aaeffa76eef34cc0a383ff658c0262136
Jeroen de Haan <info.jdehaan@gmail.com>
2020-09-02 10:00:05 +0200

    Added pickup system
    When the player picks up a sphere (placeholder) the player gets 10, 25 or 50 coins. There are 3 different types of pickups that can be set in the editor by giving the object a tag.

---
+++ Pasta Pesto Game/Assets/Scripts/PickUpSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSystem : MonoBehaviour
+{
+    [SerializeField]
+    private float _moneyTotal = 0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PickUp")
+            pickUp(10, other.gameObject);
+        else if (other.tag == "PickUp1")
+            pickUp(25, other.gameObject);
+        else if (other.tag == "PickUp2")
+            pickUp(50, other.gameObject);
+    }
+
+    private void pickUp(float pMoney, GameObject pGameObject)
+    {
+        _moneyTotal += pMoney;
+        Destroy(pGameObject);
+    }
+
+    private void Update()
+    {
+        // Used for testing
+        if (Input.GetKeyDown(KeyCode.C))
+            _moneyTotal += 50f;
+    }
+}
