0cdf3165de2ed7c347c402b0b0076439854902ab
Jeroen de Haan <info.jdehaan@gmail.com>
2020-09-08 11:00:19 +0200

    Merge Reinier & Jeroen branches

--- Pasta Pesto Game/Assets/Scripts/PickUpSystem.cs
+++ Pasta Pesto Game/Assets/Scripts/PickUpSystem.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PickUpSystem : MonoBehaviour
 {
-    [SerializeField]
-    private float _moneyTotal = 0f;
+    private GameObject _menu;
+
+    private void Awake()
+    {
+        _menu = GameObject.Find("UI_Shop");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,14 +23,135 @@ public class PickUpSystem : MonoBehaviour
 
     private void pickUp(float pMoney, GameObject pGameObject)
     {
-        _moneyTotal += pMoney;
+        _menu.GetComponent<ShopSystem>().SetMoneyAmount(pMoney);
         Destroy(pGameObject);
     }
-
-    private void Update()
-    {
-        // Used for testing
-        if (Input.GetKeyDown(KeyCode.C))
-            _moneyTotal += 50f;
-    }
 }
+
+
+
+//public class PickUpSystem : MonoBehaviour
+//{
+//    [SerializeField]
+//    private float _moneyTotal = 0f;
+//    private GameObject _UIShop;
+
+//    private void Awake()
+//    {
+//        _UIShop = GameObject.Find("UI_Shop");
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.tag == "PickUp")
+//            pickUp(10, other.gameObject);
+//        else if (other.tag == "PickUp1")
+//            pickUp(25, other.gameObject);
+//        else if (other.tag == "PickUp2")
+//            pickUp(50, other.gameObject);
+//    }
+
+//    private void pickUp(float pMoney, GameObject pGameObject)
+//    {
+//        //_moneyTotal += pMoney;
+//        _UIShop.GetComponent<ShopSystem>().SetMoney(pMoney);
+//        Destroy(pGameObject);
+//    }
+
+//    private void Update()
+//    {
+//        // Used for testing
+//        if (Input.GetKey(KeyCode.C))
+//            _moneyTotal += 1000f;
+//    }
+//}
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//<<<<<<< HEAD
+//=======
+//using UnityEngine.UI;
+//>>>>>>> Reinier
+
+//public class PickUpSystem : MonoBehaviour
+//{
+//    [SerializeField]
+//    private float _moneyTotal = 0f;
+//<<<<<<< HEAD
+//=======
+//    public GameObject UI;
+//    ShopSystem shop;
+
+//    void Awake()
+//    {
+//        shop = UI.GetComponent<ShopSystem>();
+//    }
+//>>>>>>> Reinier
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.tag == "PickUp")
+//<<<<<<< HEAD
+//            pickUp(10, other.gameObject);
+//        else if (other.tag == "PickUp1")
+//            pickUp(25, other.gameObject);
+//        else if (other.tag == "PickUp2")
+//            pickUp(50, other.gameObject);
+//=======
+//        {
+//            pickUp(10, other.gameObject);
+//            Debug.Log("Picked up an object worth: 10 points");
+//        }
+//        else if (other.tag == "PickUp1")
+//        {
+//            pickUp(25, other.gameObject);
+//            Debug.Log("Picked up an object worth: 10 points");
+//        }
+//        else if (other.tag == "PickUp2")
+//        {
+//            pickUp(50, other.gameObject);
+//            Debug.Log("Picked up an object worth: 10 points");
+//        }
+//>>>>>>> Reinier
+//    }
+
+//    private void pickUp(float pMoney, GameObject pGameObject)
+//    {
+//        _moneyTotal += pMoney;
+//        Destroy(pGameObject);
+//    }
+
+//<<<<<<< HEAD
+//    private void Update()
+//    {
+//        // Used for testing
+//        if (Input.GetKeyDown(KeyCode.C))
+//            _moneyTotal += 50f;
+//=======
+//    void Update()
+//    {
+//        // Testing purpose: Add money for testing
+//        if (Input.GetKey(KeyCode.C)) _moneyTotal += 1000f;
+//        //if (Input.GetKeyDown(KeyCode.C)) moneyCount += 10f;
+
+
+//        if (shop != null)
+//        SetPlayerMoney(_moneyTotal);
+//    }
+
+
+//    // A simple getter-function to get PlayerMoney.
+//    public float GetPlayerMoney()
+//    {
+//        _moneyTotal = shop.GetMoney();
+//        return _moneyTotal;
+//    }
+
+//    // A simple setter-function to set PlayerMoney.
+//    public void SetPlayerMoney(float pMoney)
+//    {
+//        _moneyTotal = pMoney;
+//>>>>>>> Reinier
+//    }
+//}
--- Pasta Pesto Game/Assets/Scripts/ShopSystem.cs
+++ Pasta Pesto Game/Assets/Scripts/ShopSystem.cs
@@ -8,7 +8,7 @@ using CodeMonkey.Utils;
 public class ShopSystem : MonoBehaviour
 {
     [Tooltip("The amount of money you have")] [SerializeField] private float moneyCount = 0f;
-    [Tooltip("The multiplier for upgrades")] [SerializeField] private float multiplier = 1.02f;
+    [Tooltip("The multiplier for upgrades")] [SerializeField] private float multiplier = 1.15f;
     private Transform shopItemTemplate;
     private Transform container;
     private Transform money;
@@ -187,12 +187,22 @@ public class ShopSystem : MonoBehaviour
 
     private void Update()
     {
-        // Testing purpose: Add money for testing
-        if (Input.GetKey(KeyCode.C)) moneyCount += 1000f;
-        //if (Input.GetKeyDown(KeyCode.C)) moneyCount += 10f;
-
+        GetMoney();
         // Update text with money-count
         money.GetComponent<TextMeshProUGUI>().SetText("Your influence: " + moneyCount.ToString("F2"));
+        //SetMoney(moneyCount);
     }
 
+
+    // A simple getter-function to get PlayerMoney.
+    public float GetMoney()
+    {
+        return moneyCount;
+    }
+
+    // A simple setter-function to set PlayerMoney.
+    public void SetMoneyAmount(float pMoneyAmount)
+    {
+        moneyCount += pMoneyAmount;
+    }
 }
