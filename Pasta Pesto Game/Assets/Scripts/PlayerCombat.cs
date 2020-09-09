56aebf2f96e7cc2a7a94cf252adf3d087d06652d
Jeroen de Haan <info.jdehaan@gmail.com>
2020-09-09 15:48:20 +0200

    Added combat system
    Added the combat system, still need to implement animations

--- Pasta Pesto Game/Assets/Scripts/PlayerCombat.cs
+++ Pasta Pesto Game/Assets/Scripts/PlayerCombat.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    
-=======
     public Transform AttackPoint;
     public float AttackRange = 0.5f;
     public LayerMask EnemyLayers;
 
->>>>>>> Stashed changes
     // Update is called once per frame
     void Update()
     {
@@ -23,10 +19,6 @@ public class PlayerCombat : MonoBehaviour
 
     private void attack()
     {
-<<<<<<< Updated upstream
-        
-        
-=======
         Collider[] hitEnemies = Physics.OverlapSphere(AttackPoint.position, AttackRange, EnemyLayers);
 
         foreach (Collider enemy in hitEnemies)
@@ -36,18 +28,11 @@ public class PlayerCombat : MonoBehaviour
         }
     }
 
-    private void handleEnemyDeath(Collider pEnemy)
-    {
-        //pEnemy.transform.position.y += .5f;
-        //pEnemy.transform.eulerAngles = new Vector3(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
-    }
-
     private void OnDrawGizmosSelected()
     {
         if (AttackPoint == null)
             return;
 
         Gizmos.DrawWireSphere(AttackPoint.position, AttackRange);
->>>>>>> Stashed changes
     }
 }
